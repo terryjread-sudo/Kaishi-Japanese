3b6178ba2e9f2a31d397ea7b32b3c6cccfed46f7
terryjread-sudo <terryjread@gmail.com>
2026-07-26 11:19:05 +0300

    V4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
+// Kaishi Quest v4.0.0
+// Offline caching is intentionally disabled while the direct-image release is validated.
 self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',e=>e.waitUntil(
- caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())
-));
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
