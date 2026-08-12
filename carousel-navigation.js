9ea30fab314717920415126a38c9d224e80cec43
terryjread-sudo <terryjread@gmail.com>
2026-08-13 00:11:39 +0100

    Release mobile dashboard as v11.8.2

--- carousel-navigation.js
+++ carousel-navigation.js
@@ -8,7 +8,7 @@
  * controls make the two destinations discoverable and accessible.
  */
 (() => {
-  const RELEASE='11.8.1';
+  const RELEASE='11.8.2';
   const $=(s,r=document)=>r.querySelector(s);
 
   function ensureStyles(){
--- release-manager.js
+++ release-manager.js
@@ -15,7 +15,7 @@
  * the same value in index.html and service-worker.js.
  */
 (() => {
-  const CURRENT_VERSION='11.8.1';
+  const CURRENT_VERSION='11.8.2';
   const CACHE_PREFIXES=['kaishi-shell-','kaishi-images-'];
   let refreshing=false;
 
