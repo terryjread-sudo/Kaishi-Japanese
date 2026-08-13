ae98fcf0bd20aa20231fe8336e54b452fe322d24
terryjread-sudo <terryjread@gmail.com>
2026-08-13 04:59:06 +0100

    Add 50 illustrated vocabulary mnemonics

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VERSION='11.8.7';
+const VERSION='11.8.8';
 const SHELL_CACHE=`kaishi-shell-${VERSION}`;
 const IMAGE_CACHE=`kaishi-images-${VERSION}`;
 const MAX_RUNTIME_IMAGES=350;
