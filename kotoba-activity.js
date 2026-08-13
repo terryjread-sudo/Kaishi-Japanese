b35f985c7519f35fb3d6e5b92d0c397f486c5221
terryjread-sudo <terryjread@gmail.com>
2026-08-13 05:54:00 +0100

    Replace Theatre mouth overlay with cinematic frames

--- kotoba-activity.js
+++ kotoba-activity.js
@@ -8,7 +8,7 @@
  * renderer so a re-render cannot permanently remove the new activity.
  */
 (() => {
-  const RELEASE_VERSION='11.8.9';
+  const RELEASE_VERSION='11.8.10';
   const REQUIRED_WORDS=8;
 
   function introducedCount(){
