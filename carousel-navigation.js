99d619db5c7113edcb8cda6e4e9f90da8a46c009
terryjread-sudo <terryjread@gmail.com>
2026-08-15 12:51:04 +0100

    Add two interactive manga stories

--- carousel-navigation.js
+++ carousel-navigation.js
@@ -8,7 +8,7 @@
  * controls make the two destinations discoverable and accessible.
  */
 (() => {
-  const RELEASE='11.8.13';
+  const RELEASE='11.8.14';
   const $=(s,r=document)=>r.querySelector(s);
 
   function ensureStyles(){
--- kotoba-activity.js
+++ kotoba-activity.js
@@ -8,7 +8,7 @@
  * renderer so a re-render cannot permanently remove the new activity.
  */
 (() => {
-  const RELEASE_VERSION='11.8.13';
+  const RELEASE_VERSION='11.8.14';
   const REQUIRED_WORDS=8;
 
   function introducedCount(){
